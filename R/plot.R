a10a00863e5c0c03900b965ec2322b3bcb5f59ac
Alberto Pessia <albertopessia@users.noreply.github.com>
2022-09-13 12:21:00 +0300

    Fix bugs in plots

--- R/plot.R
+++ R/plot.R
@@ -259,8 +259,9 @@ plot.drdalist <- function(x, ...) {
 
   tmp <- vapply(params, function(w) w$xlim, numeric(2))
 
-  j <- which.max(tmp[2, ])
-  xlim <- c(min(tmp[1, ]), tmp[2, j])
+  j1 <- which.min(tmp[1, ])
+  j2 <- which.max(tmp[2, ])
+  xlim <- c(tmp[1, j1], tmp[2, j2])
 
   tmp <- vapply(params, function(w) w$ylim, numeric(2))
   ylim <- c(min(tmp[1, ]), max(tmp[2, ]))
@@ -273,32 +274,56 @@ plot.drdalist <- function(x, ...) {
   )
 
   if (plot_type == 1) {
-    axis(1, at = params[[j]]$x_axis_ticks, labels = params[[j]]$x_axis_labels)
+    axis(1, at = params[[j2]]$x_axis_ticks, labels = params[[j2]]$x_axis_labels)
   } else if (plot_type == 2) {
+    # on the left side of the plot we must use the smallest values
     axis(
-      1, at = params[[j]]$x_axis_ticks_1, labels = params[[j]]$x_axis_labels_1
-    )
-    axis(
-      1, at = params[[j]]$x_axis_ticks_2, labels = params[[j]]$x_axis_labels_2
+      1, at = params[[j1]]$x_axis_ticks_1, labels = params[[j1]]$x_axis_labels_1
     )
 
+    # default ticks are those of "j1"
+    tks_1 <- params[[j1]]$x_axis_ticks_2
+    lbl_1 <- params[[j1]]$x_axis_labels_2
+
+    # does "j2" have extra ticks to add?
+    # remove the "gap" tick because we are using that of "j1"
+    tks_2 <- params[[j2]]$x_axis_ticks_2[-1]
+    lbl_2 <- params[[j2]]$x_axis_labels_2[-1]
+
+    idx <- !(tks_2 %in% tks_1)
+    if (any(idx)) {
+      tks_1 <- c(tks_1, tks_2[idx])
+      lbl_1 <- c(lbl_1, lbl_2[idx])
+
+      ord <- order(tks_1)
+      tks_1 <- tks_1[ord]
+      lbl_1 <- lbl_1[ord]
+    }
+
+    axis(1, at = tks_1, labels = lbl_1)
+
     axis(
-      1, at = params[[j]]$x_axis_ticks_1[2], labels = FALSE, tcl = -par("tcl")
+      1, at = params[[j1]]$x_axis_ticks_1[2], labels = FALSE, tcl = -par("tcl")
     )
     axis(
-      1, at = params[[j]]$x_axis_ticks_2[1], labels = FALSE, tcl = -par("tcl")
+      1, at = params[[j1]]$x_axis_ticks_2[1], labels = FALSE, tcl = -par("tcl")
     )
   }
 
-  axis(1, at = params[[j]]$x_axis_minor, labels = FALSE, tcl = par("tcl") * 0.5)
+  axis(
+    1,
+    at = sort(unique(c(params[[j1]]$x_axis_minor, params[[j2]]$x_axis_minor))),
+    labels = FALSE, tcl = par("tcl") * 0.5
+  )
+
   axis(2, at = pretty(ylim))
 
-  box_x <- par("usr")[params[[j]]$box$x]
-  box_y <- par("usr")[params[[j]]$box$y]
+  box_x <- par("usr")[params[[j1]]$box$x]
+  box_y <- par("usr")[params[[j1]]$box$y]
 
-  if (!is.null(params[[j]]$box$z)) {
-    box_x[1] <- params[[j]]$box$z[1]
-    box_x[10] <- params[[j]]$box$z[2]
+  if (!is.null(params[[j1]]$box$z)) {
+    box_x[1] <- params[[j1]]$box$z[1]
+    box_x[10] <- params[[j1]]$box$z[2]
   }
 
   lines(x = box_x, y = box_y)
@@ -433,6 +458,12 @@ plot_params.logistic <- function(x, base, xlim, ylim) {
     xv <- xv[idx]
     wv <- wv[idx]
   }
+
+  # make sure data is sorted according to `xv`
+  ord <- order(xv, yv, wv)
+  xv <- xv[ord]
+  yv <- yv[ord]
+  wv <- wv[ord]
 
   if (is.null(xlim)) {
     xlim <- extendrange(xv, f = 0.08)
@@ -651,6 +682,12 @@ plot_params.loglogistic <- function(x, base, xlim, ylim) {
     xv <- xv[idx]
     wv <- wv[idx]
   }
+
+  # make sure data is sorted according to `xv`
+  ord <- order(xv, yv, wv)
+  xv <- xv[ord]
+  yv <- yv[ord]
+  wv <- wv[ord]
 
   len <- length(xv)
   zero_x <- xv == 0
